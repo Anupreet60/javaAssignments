5f7957240358ed194998467dd9b6341187284225
Anupreet Kaur <W0860922@myscc.ca>
2025-06-12 14:46:30 -0400

    Add lizard and spock to game

--- src/Lab4.java
+++ src/Lab4.java
@@ -1,44 +1,54 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Lab4 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        String[] options = {"scissors", "rock", "paper", "lizard", "spock"};
 
-        //Generate a Random Integer
-        int computer = (int)(Math.random() * 3);
+        int userScore = 0;
+        int computerScore = 0;
 
-        //Prompt the user to enter a integer
-        System.out.print("scissor (0), rock (1), paper (2): ");
-        int user = input.nextInt();
+        while (true) {
+            System.out.print("\nChoose your move (scissors, rock, paper, lizard, spock), or type 'exit' to quit: ");
+            String playerChoice = scan.nextLine().trim().toLowerCase();
 
-        System.out.print("The computer is ");
-        switch (computer)
-        {
-            case 0: System.out.print("scissor."); break;
-            case 1: System.out.print("rock."); break;
-            case 2: System.out.print("paper.");
+            if (playerChoice.equals("exit")) {
+                System.out.println("Final Score - You: " + userScore + ", Computer: " + computerScore);
+                System.out.println("Thanks for playing!");
+                break;
+            }
+
+            if (!playerChoice.equals("scissors") && !playerChoice.equals("rock") &&
+                    !playerChoice.equals("paper") && !playerChoice.equals("lizard") &&
+                    !playerChoice.equals("spock")) {
+                System.out.println("Invalid input. Please enter 'scissors', 'rock', 'paper', 'lizard', 'spock', or 'exit'.");
+                continue;
+            }
+
+            String computerChoice = options[random.nextInt(options.length)];
+            System.out.println("Computer chose: " + computerChoice);
+
+            if (playerChoice.equals(computerChoice)) {
+                System.out.println("It's a tie!");
+            } else if (
+                    (playerChoice.equals("scissors") && (computerChoice.equals("paper") || computerChoice.equals("lizard"))) ||
+                            (playerChoice.equals("rock") && (computerChoice.equals("scissors") || computerChoice.equals("lizard"))) ||
+                            (playerChoice.equals("paper") && (computerChoice.equals("rock") || computerChoice.equals("spock"))) ||
+                            (playerChoice.equals("lizard") && (computerChoice.equals("spock") || computerChoice.equals("paper"))) ||
+                            (playerChoice.equals("spock") && (computerChoice.equals("scissors") || computerChoice.equals("rock")))
+            ) {
+                System.out.println("You win!");
+                userScore++;
+            } else {
+                System.out.println("You lose!");
+                computerScore++;
+            }
+
+            System.out.println("Score => You: " + userScore + ", Computer: " + computerScore);
         }
 
-        System.out.print(" You are ");
-        switch (user)
-        {
-            case 0: System.out.print("scissor"); break;
-            case 1: System.out.print("rock"); break;
-            case 2: System.out.print("paper ");
-        }
-
-       //Display Results
-        if (computer == user)
-            System.out.println(" too. It is a draw");
-        else
-        {
-            boolean win = (user == 0 && computer == 2) ||
-                    (user == 1 && computer == 0) ||
-                    (user == 2 && computer == 1);
-            if (win)
-                System.out.println(". You won");
-            else
-                System.out.println(". You lose");
-        }
+        scan.close();
     }
 }
